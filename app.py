7f2adb91b8098903ed73e5b7921af14aecbd8d34
vladtsap <vldzpp@gmail.com>
2020-08-12 13:58:16 +0300

    get ready for implementing mongoDB

--- app.py
+++ app.py
@@ -1,35 +1,52 @@
 from flask import Flask, request, jsonify
-from pymongo import MongoClient
+import pymongo
 import boto3
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb', 27017)
-db = client.tododb
+# db_client = pymongo.MongoClient('mongo', 27017)  # for running in Docker
+db_client = pymongo.MongoClient('mongodb://localhost:27017/')  # for running in PyCharm
 
 
 @app.route('/')
 def index():
+	"""Start page with hints"""
 	return '''<h1>Hello, World!</h1><p>Send GET request to /api with 'id' and 'secret' params.</p>'''
 
 
 @app.errorhandler(400)
 def page_missing_params(e):
+	"""Error page when missing parameter"""
 	return "<h1>400 Bad Request</h1><p>Expecting 2 params but got 1.</p>", 400
 
 
 @app.errorhandler(404)
 def page_not_found(e):
+	"""Error when page is not exist"""
 	return "<h1>404 Not Found</h1><p>The resource could not be found.</p>", 404
 
 
 @app.errorhandler(502)
 def page_invalid_params(e, info):
+	"""Error when parameters are invalid"""
 	return "<h1>502 Bad Gateway</h1><p>{}</p>".format(info), 502
+
+
+def vpc_info(key_id, secret_key):
+	"""Get info about VPCs"""
+	boto3_client = boto3.client(
+		'ec2',
+		aws_access_key_id=key_id,
+		aws_secret_access_key=secret_key,
+		region_name='eu-central-1'
+	)
+
+	return boto3_client.describe_vpcs()
 
 
 @app.route('/api', methods=['GET'])
 def api():
+	"""Process API requests"""
 	query_parameters = request.args
 
 	key_id = query_parameters.get('id')
@@ -37,14 +54,7 @@ def api():
 
 	if key_id and secret_key:
 		try:
-			client = boto3.client(
-				'ec2',
-				aws_access_key_id=key_id,
-				aws_secret_access_key=secret_key,
-				region_name='eu-central-1'
-			)
-
-			results = client.describe_vpcs()
+			results = vpc_info(key_id, secret_key)
 			return jsonify(results)
 
 		except Exception as e:
